6866229a7f8c266fe684a7f2fe4140e39e73d395
medprohtml <2001120500400@ofppt-edu.ma>
2023-10-18 21:08:07 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Body from './compenets/Body'
 import Bodypart2 from './compenets/Bodypart2'
 import Navbar from './compenets/navbar'
-
+import image from './assets/1290174.jpg'
 function App() {
   
 
@@ -11,8 +11,14 @@ function App() {
   
       <div >
           <Navbar/>
-          <Body/>
-          <Bodypart2/>
+      <Body
+      derection=''  
+       title="Your favorite Food Delivered hot & Fresh"
+       info="Healthy switcher chefs do all the prep work, like peeling, chopping & marinating, so you can cook a fresh meal."
+       img={image} // You should replace `imageSrc` with the actual image source URL
+     />
+
+          
       </div>
      
     
--- src/compenets/Body.jsx
+++ src/compenets/Body.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { AiFillCaretRight } from "react-icons/ai";
-import image from '../assets/1290174.jpg'
 
 
-function Body() {
+function Body(props) {
+
+      const {title,info,img,derection }=props
+
   return (
-    <div>
-        <main className=' p-20 flex justify-between'>
-            <div className='info max-w-md'>
-               <h1 className=' text-4xl font-bold'> Your favorite Food Delivred hot & Fresh</h1>
-               <p className='parinfo font-normal text-sm mt-3 '>Healty switcher chefs do all the prep work, like pedding ,chopping & mirinating 
-               ,so you can cook a frech food
+    <div className=''>
+        <main 
+        className={` ${derection=='row-reverse'?'flex-row-reverse':''} p-20 flex justify-between`}>
+            <div
+             className='info max-w-md'>
+               <h1
+                className=' text-4xl font-bold'>{title} </h1>
+               <p 
+               className='parinfo font-normal text-sm mt-3 '>
+                {info}
                 </p>
 
-                <button className=' font-mono text-xl  flex mt-6 w-48 rounded-xl bg-orange-400 p-3'>Order Now <AiFillCaretRight className=' ms-2 mt-1.5 text-xl '/>
+                <button
+                 className=' font-mono text-xl  flex mt-6 w-48 rounded-xl bg-orange-400 p-3'>Order Now <AiFillCaretRight className=' ms-2 mt-1.5 text-xl '/>
                 </button>
             </div>
-            <img src={image} className=' w-1/2 rounded-2xl' alt="" />
+            <img src={img} className=' w-1/2 rounded-2xl' alt="" />
         </main>
     </div>
   )
